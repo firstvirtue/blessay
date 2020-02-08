7b137102d50a0873b037e6bd63e0fd89d72e789f
Jake Lee <jake@iropke.com>
2020-02-08 12:50:13 +0900

    Add local register validation

--- src/api/auth/auth.ctrl.js
+++ src/api/auth/auth.ctrl.js
@@ -1,6 +1,20 @@
 const Joi = require('joi');
 
 exports.localRegister = async (ctx) => {
+
+  const schema = Joi.object().keys({
+    username: Joi.string().alphanum().min(4).max(15).required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required().min(6)
+  });
+
+  const result = Joi.validate(ctx.request.body, schema);
+
+  if(result.error) {
+    ctx.status = 400;
+    return;
+  }
+  
   ctx.body = 'register';
 }
 
