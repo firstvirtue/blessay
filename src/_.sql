b1f3cf24a540dad75f4a78bd6dd77845addf07c4
Jake Lee <firstvirtue@gmail.com>
2020-09-02 01:31:11 +0900

    Append sql code

---
+++ src/_.sql
@@ -0,0 +1,49 @@
+DROP TABLE IF EXISTS account;
+CREATE TABLE account(
+  id VARCHAR(50) PRIMARY KEY,
+  username VARCHAR (50) NOT NULL,
+  email VARCHAR (355) UNIQUE,
+  password VARCHAR (100) NOT NULL,
+  created_on TIMESTAMP NOT NULL,
+  last_login TIMESTAMP
+);
+
+-- milestone 1
+-- article
+DROP TABLE IF EXISTS article;
+CREATE TABLE article(
+  id serial PRIMARY KEY,
+  title VARCHAR (500) NOT NULL,
+  writer VARCHAR (400) NOT NULL,
+  thumbnail VARCHAR (200),
+  description VARCHAR (400),
+  category VARCHAR,
+  published BOOLEAN NOT NULL,
+  created_on TIMESTAMP NOT NULL,
+  updated_on TIMESTAMP NOT NULL
+);
+-- block
+DROP TABLE IF EXISTS article_block;
+CREATE TABLE article_block(
+  id serial PRIMARY KEY,
+  article_id serial NOT NULL,
+  type VARCHAR (100) NOT NULL,
+  -- content VARCHAR (2000),
+  content_data json,
+  optional VARCHAR (1000),
+  created_on TIMESTAMP NOT NULL,
+  updated_on TIMESTAMP NOT NULL
+  -- tag array?
+);
+
+-- category
+DROP TABLE IF EXISTS category;
+CREATE TABLE category(
+  id serial PRIMARY KEY,
+  content VARCHAR (500) NOT NULL
+);
+
+-- milestone 2
+-- group -- group - category
+-- role
+-- block tag
